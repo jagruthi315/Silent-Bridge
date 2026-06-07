e0c6eb4fb2a026cfeb676ffa8aa92c51d3afab5a
Jagruthi Sachin Poondrrikaksha <173657547+jagruthi315@users.noreply.github.com>
2026-06-07 23:45:33 +0530

    Add print statement and reorganize code in hand_detection.py
    reconfigure the virtual environment as per the needs of the project and added drawing lines to see if landmarks are working and connected it with the webcam

--- Jagruthi/hand_detection.py
+++ Jagruthi/hand_detection.py
@@ -1,11 +1,16 @@
 import cv2
 import mediapipe as mp
+
+
+print(hasattr(mp, "solutions"))
+
 # Webcam connection
 cap = cv2.VideoCapture(0)
 
 # MediaPipe Hands setup
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
 
 while True:
     success, frame = cap.read()
@@ -25,6 +30,9 @@ while True:
 
         for hand_landmarks in results.multi_hand_landmarks:
 
+            mp_draw.draw_landmarks(frame,
+            hand_landmarks,mp_hands.HAND_CONNECTIONS)
+
             row = []
 
             for landmark in hand_landmarks.landmark:
@@ -39,9 +47,5 @@ while True:
     if cv2.waitKey(1) == 27 or cv2.waitKey(1) == ord('q'):
         break
 
-
-
 cap.release()
 cv2.destroyAllWindows()
-
-
