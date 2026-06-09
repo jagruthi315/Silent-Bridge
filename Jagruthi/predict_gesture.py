fac8066ccd1d75cc2947b424ee382b082febbf8e
Jagruthi Sachin Poondrrikaksha <173657547+jagruthi315@users.noreply.github.com>
2026-06-09 13:57:12 +0530

    Add files via upload

---
+++ Jagruthi/predict_gesture.py
@@ -0,0 +1,31 @@
+import pickle
+
+# Load trained model once
+with open("gesture_model.pkl", "rb") as file:
+    model = pickle.load(file)
+
+
+def predict(row):
+
+    prediction = model.predict([row])[0]
+
+    confidence = max(
+        model.predict_proba([row])[0]
+    )
+
+   
+    return prediction,float( round(confidence * 100, 2)) 
+
+# TEST
+# dummy_row = [0] * 63
+
+# result = predict(dummy_row)
+
+# print(result)
+
+
+if __name__ == "__main__":
+
+    dummy_row = [0] * 63
+
+    print(predict(dummy_row))
